70ddbef2b0a622480ea1ca24554f77a9d49e2025
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-10-23 16:38:20 +0800

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,5 +1,5 @@
+int Waitmsec = 100;
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
   pinMode(D5, OUTPUT);
@@ -7,20 +7,83 @@ void setup() {
   pinMode(D7, OUTPUT);
 }
 
+void One() {
+  digitalWrite(D3, LOW);
+  digitalWrite(D7, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D7, LOW);
+  digitalWrite(D6, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D6, LOW);
+  digitalWrite(D5, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D5, LOW);
+  digitalWrite(D4, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D4, LOW);
+  digitalWrite(D3, HIGH);
+  delay(Waitmsec);
+}
+
+void Two() {
+  digitalWrite(D7, LOW);
+  digitalWrite(D3, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D4, LOW);
+  digitalWrite(D5, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, HIGH);
+  delay(Waitmsec);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, HIGH);
+  delay(Waitmsec);
+}
+void Three() {
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(D7, HIGH);
+    digitalWrite(D5, HIGH);
+    digitalWrite(D3, HIGH);
+    digitalWrite(D6, LOW);
+    digitalWrite(D4, LOW);
+    delay(Waitmsec);
+    digitalWrite(D7, LOW);
+    digitalWrite(D5, LOW);
+    digitalWrite(D3, LOW);
+    digitalWrite(D6, HIGH);
+    digitalWrite(D4, HIGH);
+    delay(Waitmsec);
+  }
+  digitalWrite(D7, LOW);
+  digitalWrite(D6, LOW);
+  digitalWrite(D5, LOW);
+  digitalWrite(D4, LOW);
+  digitalWrite(D3, LOW);
+}
+
+void Four() {
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(D7, HIGH);
+    digitalWrite(D5, HIGH);
+    digitalWrite(D3, HIGH);
+    digitalWrite(D6, HIGH);
+    digitalWrite(D4, HIGH);
+    delay(Waitmsec);
+    digitalWrite(D7, LOW);
+    digitalWrite(D5, LOW);
+    digitalWrite(D3, LOW);
+    digitalWrite(D6, LOW);
+    digitalWrite(D4, LOW);
+    delay(Waitmsec);
+  }
+}
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  
-  digitalWrite(D3, HIGH); 
-  digitalWrite(D4, HIGH); 
-  digitalWrite(D5, HIGH); 
-  digitalWrite(D6, HIGH); 
-  digitalWrite(D7, HIGH);   
-  delay(100);                       
-  digitalWrite(LED_BUILTIN, LOW);    
-  digitalWrite(D3, LOW); 
-  digitalWrite(D4, LOW); 
-  digitalWrite(D5, LOW); 
-  digitalWrite(D6, LOW); 
-  digitalWrite(D7, LOW);    
-  delay(100);                      
+  One();
+  Two();
+  Three();  
+  Four();
 }
